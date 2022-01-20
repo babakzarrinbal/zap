d57b0d1e9b9f9a9c3802fe581c5ba56eba052ebd
Babak Zarrinbal <babak.zarrinbal@gmail.com>
2022-01-20 20:49:19 +0330

    rollup added

---
+++ modules/00-Core/dist/browser.ts
@@ -0,0 +1,8 @@
+/**
+   * @license
+   * author: babak.zarrinbal
+   * core.js v1.0.1
+   * Released under the ISC license.
+   */
+var core=function(){"use strict";return Object.freeze({__proto__:null,initiated:!0})}();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnJvd3Nlci50cyIsInNvdXJjZXMiOlsiLi4vc3JjL0FQSS9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgaW5pdGlhdGVkICA9IHRydWU7XHJcbiJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7aUZBQTBCIn0=
