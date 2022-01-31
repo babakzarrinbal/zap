e219541f3083f5ad6c77a93fbd4e535e03ef76e5
Babak Zarrinbal <babak.zarrinbal@gmail.com>
2022-01-31 13:21:28 +0330

    init commit

---
+++ modules/00-Core/test/API/endpoint.test.ts
@@ -0,0 +1,9 @@
+import {create,getOne} from "@src/API/endpoints"
+// TODO add delete update to the test create mock data for entry point ...
+
+describe('testing initiation of project', (): void => {
+  // test('const', (): void => {
+  //   const resp: boolean = ;
+  //   expect(resp).toBe(true);
+  // });
+});
--- modules/00-Core/test/test.ts
+++ modules/00-Core/test/test.ts
@@ -1,10 +1,12 @@
-import  {endPoint} from "@src/index";
-(async function() {
-  const e = await endPoint.getOne("./sqlite.sqlite",1)
-  console.log('test',e)
-  // console.log(await endPoint.create(database))
-  // const newId = await queryEndpoint.insertEndpoint(
-  //   database,1,1,1,1,1,1,1
-  // )
-  // console.log(await queryEndpoint.selectEndpoint(database,newId))
-})()
+// import  {endPoint} from "@src/index";
+// (async function() {
+//   const e = await endPoint.getOne("./sqlite.sqlite",1)
+//   console.log('test',e)
+//   // console.log(await endPoint.create(database))
+//   // const newId = await queryEndpoint.insertEndpoint(
+//   //   database,1,1,1,1,1,1,1
+//   // )
+//   // console.log(await queryEndpoint.selectEndpoint(database,newId))
+// })()
+
+console.log(require('os').homedir())
