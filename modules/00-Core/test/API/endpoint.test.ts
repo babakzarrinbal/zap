dbfd45cf6440278310bb190e3d0f9d8eea8b530f
Babak Zarrinbal <babak.zarrinbal@gmail.com>
2022-02-03 13:59:15 +0330

    mehrad onboarding

--- modules/00-Core/test/API/endpoint.test.ts
+++ modules/00-Core/test/API/endpoint.test.ts
@@ -1,9 +1,10 @@
 import {create,getOne} from "@src/API/endpoints"
 // TODO add delete update to the test create mock data for entry point ...
-
+const testresult={}
 describe('testing initiation of project', (): void => {
   // test('const', (): void => {
   //   const resp: boolean = ;
-  //   expect(resp).toBe(true);
+  //   expect(resp).toEqual({});
   // });
 });
+
